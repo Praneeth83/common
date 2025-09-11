68084c3e5bf3796ebd7fe7cc3328ecd5cee96b87
Praneeth83 <167945790+Praneeth83@users.noreply.github.com>
2025-09-11 18:25:51 +0530

    first commit

---
+++ db/db.go
@@ -0,0 +1,21 @@
+package common // important: package name must match the module import
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Global DB variable
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "postgres://username:password@host:port/dbname?sslmode=require"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to database: %v", err))
+	}
+	fmt.Println("Connected to remote DB")
+}
